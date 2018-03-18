3792df65cc723ebdc5a5b60e8e4354d3f56598ca
fodhil zidane <fodhilzidane@gmail.com>
2018-03-17 22:02:50 -0400

    Add project files.

---
+++ Lunch/Models/PersonViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace Lunch.Models
+{
+    public class PersonViewModel
+    {
+        public int? PersonId { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Name")]
+        public string FullName => FirstName + " " + LastName;
+    }
+}
